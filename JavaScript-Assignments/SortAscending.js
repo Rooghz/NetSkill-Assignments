5e3b3aabe8fecc7febb0c7ed552230ff9f76c4fe
Ayan Saha <49653970+Rooghz@users.noreply.github.com>
2023-02-01 22:42:27 +0530

    Update SortAscending.js

--- JavaScript-Assignments/SortAscending.js
+++ JavaScript-Assignments/SortAscending.js
@@ -3,7 +3,10 @@ var numbers = [34, 7, 23, 32, 5, 62];
 numbers.sort(Compare);
 console.log(numbers);
 
-function Compare(a,b){
+// This function compare values and if a-b is positive then b comes before a, example 34,7 34-7=27 which is positive so it swap the position and becomes 7,34.
+// It does this untill everything sorted and for every pair a-b = positive.
+
+function Compare(a,b){  
     return a-b;
 }
 
