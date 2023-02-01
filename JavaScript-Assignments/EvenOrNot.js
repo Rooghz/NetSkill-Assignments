0bf7304c7d54f6d3cae9d5e4414eb6806ca65e9e
Ayan Saha <49653970+Rooghz@users.noreply.github.com>
2023-02-01 21:24:18 +0530

    Create EvenOrNot.js

---
+++ JavaScript-Assignments/EvenOrNot.js
@@ -0,0 +1,21 @@
+function evenOrNot(n){
+
+    if(n%2 === 0){
+        return "Even";
+    }
+    else
+        return "Not Even";
+}
+
+console.log(evenOrNot(3));
+console.log(evenOrNot(4));
+
+
+/*
+
+Output - 
+
+Not Even
+Even
+
+*/
