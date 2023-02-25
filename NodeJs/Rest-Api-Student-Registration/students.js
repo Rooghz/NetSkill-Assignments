0db9eccb2780711beaa7f5428c2cb5282153919a
Ayan Saha <49653970+Rooghz@users.noreply.github.com>
2023-02-25 17:42:37 +0530

    Update students.js

--- NodeJs/Rest-Api-Student-Registration/students.js
+++ NodeJs/Rest-Api-Student-Registration/students.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const studentSchema = new mongoose.Schema({
-    name : {
+    name: {
         type:String,
         required:true,
         minlength:3
